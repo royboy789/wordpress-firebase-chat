356a1e9a9d3ed2c2850c51d5ef9b6db488bb88ec
Roy Sivan <roy.x.sivan.-ND@disney.com>
2015-03-05 08:29:14 -0800

    resetMsg constant name

--- js/chatroom_app.js
+++ js/chatroom_app.js
@@ -15,7 +15,7 @@ chat_app.controller('chat_controller', [ '$rootScope', '$scope', '$http', '$fire
 		});
 	}
 	
-	$scope.resetMsg = function() {
+	$scope.resetMsg = function( name ) {
 		$scope.msg = {
 			msg: ''
 		}
@@ -23,13 +23,15 @@ chat_app.controller('chat_controller', [ '$rootScope', '$scope', '$http', '$fire
 	$scope.resetMsg();
 	$scope.newChat = function() {
 		
+		var name = $scope.msg.name;
+		
 		if( !$scope.fireChat.chat ) {
 			$scope.fireChat.chat = [$scope.msg];
 		} else {
 			$scope.fireChat.chat.push( $scope.msg );
 		}
 		
-		$scope.resetMsg();
+		$scope.resetMsg( name );
 		
 	}
 	
