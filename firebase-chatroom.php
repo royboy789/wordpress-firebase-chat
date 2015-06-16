316db817b8277821bbcaf3bfe06064cc2bf86c8c
robneu <rob@wpsitecare.com>
2015-06-15 23:04:37 -0500

    Moved dep checking functions into main class

--- firebase-chatroom.php
+++ firebase-chatroom.php
@@ -14,27 +14,52 @@ define( 'CHATROOM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CHATROOM_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CHATROOM_PLUGIN_VERSION', '0.1' );
 
-require 'inc/init.php';
-require 'inc/admin-menu.php';
-require 'inc/chatroom-template.php';
-require 'inc/shortcodes.php';
+require_once CHATROOM_PLUGIN_PATH . 'inc/init.php';
+require_once CHATROOM_PLUGIN_PATH . 'inc/admin-menu.php';
+require_once CHATROOM_PLUGIN_PATH . 'inc/chatroom-template.php';
+require_once CHATROOM_PLUGIN_PATH . 'inc/shortcodes.php';
 
 class fire_chat {
 
 	function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'chat_scripts' ) );
-
 		new fireInit();
 		new chat_admin_menu();
 		new _chatroom_tpl();
 
 		$shortcode = new chatroom_shortcode();
 		$shortcode->__init();
-
+		self::wp_hooks();
 	}
 
-	function chat_scripts() {
+	protected function wp_hooks() {
+		add_action( 'admin_init', array( $this, 'wpapi_check' ) );
+		add_action( 'admin_init', array( $this, 'angular_check' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+	}
 
+	/** JSON REST API CHECK **/
+	function wpapi_check() {
+		if ( ! is_plugin_active( 'json-rest-api/plugin.php' ) ) {
+			add_action( 'admin_notices', array( $this, 'chat_wpapi_error' ) );
+		}
+	}
+
+	function wpapi_error() {
+		echo '<div class="error"><p><strong>JSON REST API</strong> must be installed and activated for the <strong>Chat Rooms</strong> plugin to work properly - <a href="https://wordpress.org/plugins/json-rest-api/" target="_blank">Install Plugin</a></p></div>';
+	}
+
+	/** ANGULARJS FOR WP CHECK **/
+	function angular_check() {
+		if ( ! is_plugin_active( 'angularjs-for-wp/plugin.php' ) ) {
+			add_action( 'admin_notices', array( $this, 'chat_angular_error' ) );
+		}
+	}
+
+	function angular_error() {
+		echo '<div class="error"><p><strong>AngularJS For WP</strong> must be installed and activated for the <strong>Chat Rooms</strong> plugin to work properly - <a href="https://wordpress.org/plugins/angularjs-for-wp/" target="_blank">Install Plugin</a></p></div>';
+	}
+
+	function scripts() {
 		wp_enqueue_script(
 			'angular-resource-library',
 			'//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular-resource.min.js',
@@ -76,34 +101,6 @@ class fire_chat {
 		);
 	}
 }
-
-
-/** JSON REST API CHECK **/
-function chat_wpapi_check() {
-	if ( ! is_plugin_active( 'json-rest-api/plugin.php' ) ) {
-		add_action( 'admin_notices', 'chat_wpapi_error' );
-	}
-}
-
-function chat_wpapi_error() {
-	echo '<div class="error"><p><strong>JSON REST API</strong> must be installed and activated for the <strong>Chat Rooms</strong> plugin to work properly - <a href="https://wordpress.org/plugins/json-rest-api/" target="_blank">Install Plugin</a></p></div>';
-}
-
-add_action( 'admin_init', 'chat_wpapi_check' );
-
-
-/** ANGULARJS FOR WP CHECK **/
-function chat_angular_check() {
-	if ( ! is_plugin_active( 'angularjs-for-wp/plugin.php' ) ) {
-		add_action( 'admin_notices', 'chat_angular_error' );
-	}
-}
-
-function chat_angular_error() {
-	echo '<div class="error"><p><strong>AngularJS For WP</strong> must be installed and activated for the <strong>Chat Rooms</strong> plugin to work properly - <a href="https://wordpress.org/plugins/angularjs-for-wp/" target="_blank">Install Plugin</a></p></div>';
-}
-
-add_action( 'admin_init', 'chat_angular_check' );
 
 
 // PLUGIN INIT
